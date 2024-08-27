c95df5ac2d8bf09888aad540e1219cbe246d48f5
Melanie Ehrlich <jmfsmof@jmfamily.com>
2024-08-27 10:52:35 -0400

    added reset game function

--- codejam2/TicTacToe.cs
+++ codejam2/TicTacToe.cs
@@ -12,6 +12,7 @@ public class TicTacToe
 
     public static void PlayGame()
     {
+        ResetGame(); // reset the game board
         int move;
         bool gameWon = false;
 
@@ -74,6 +75,12 @@ public class TicTacToe
         Console.WriteLine($" {board[3]} | {board[4]} | {board[5]} ");
         Console.WriteLine("---|---|---");
         Console.WriteLine($" {board[6]} | {board[7]} | {board[8]} ");
+    }
+
+    private static void ResetGame()
+    {
+        board = new char[] { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        currentPlayer = 'X';
     }
 
     private static bool CheckWin()
